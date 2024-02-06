39711dbdc68a3f1f9253996af1cd76c0e2b302e6
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-02-06 19:46:26 +0600

    Add responsive navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,6 +2,8 @@
 
 import React from 'react';
 import { Input } from 'antd';
+import { Navbar, Button } from 'keep-react';
+import Link from 'next/link';
 
 const { Search } = Input;
 
@@ -13,37 +15,72 @@ export default function NavBar({ searchValue }: Readonly<Props>) {
   let searchQuery = searchValue ? searchValue : undefined;
 
   return (
-    <div className="w-full bg-teal-500/20 h-16 flex">
-      <div className="flex justify-center items-center">
-        <a href="/" className="text-4xl pl-24">
-          Childe<span className="text-teal-500">Anime</span>
-        </a>
-      </div>
-      <div className="flex justify-center items-center h-16 w-full">
-        <div className="pl-24 flex gap-48 ">
-          <div className="flex gap-4">
-            <a href="#trending" className="text-3xl">
+    <Navbar fluid={true} className="bg-teal-500/50">
+      <Navbar.Container className="flex items-center justify-between">
+        <Navbar.Container className="flex items-center">
+          <Navbar.Brand>
+            <a
+              href="/"
+              className="text-sm mr-2 ml-0 pl-0 md:text-xl lg:text-3xl"
+            >
+              Childe<span className="text-teal-500">Anime</span>
+            </a>
+          </Navbar.Brand>
+          <Navbar.Divider></Navbar.Divider>
+          <Navbar.Container
+            tag="ul"
+            className="lg:flex hidden items-center justify-between gap-8"
+          >
+            <Link
+              href={'/'}
+              className="text-white text-xl hover:text-white/75 duration-300"
+            >
+              Home
+            </Link>
+            <Link
+              href={'/trending'}
+              className="text-white text-xl hover:text-white/75 duration-300"
+            >
               Trending
-            </a>
-            <a href="/popular" className="text-3xl">
-              Popular
-            </a>
-          </div>
-          <div>
-            <Search
-              placeholder="Search by Anime name..."
-              enterButton="Search"
-              size="large"
-              value={searchQuery}
-              onSearch={(value) =>
-                (window.location.href = `/search?q=${encodeURIComponent(
-                  value
-                )}`)
-              }
-            />
-          </div>
-        </div>
-      </div>
-    </div>
+            </Link>
+          </Navbar.Container>
+          <Navbar.Collapse
+            collapseType="sidebar"
+            className="bg-teal-500 text-white"
+          >
+            <Navbar.Container
+              tag="ul"
+              className="flex flex-col gap-5 text-white"
+            >
+              <Link
+                href={'/'}
+                className="text-white text-xl hover:text-white/75 duration-300"
+              >
+                Home
+              </Link>
+              <Link
+                href={'/trending'}
+                className="text-white text-xl hover:text-white/75 duration-300"
+              >
+                Trending
+              </Link>
+            </Navbar.Container>
+          </Navbar.Collapse>
+        </Navbar.Container>
+
+        <Navbar.Container className="flex gap-2">
+          <Search
+            placeholder="Search by Anime name..."
+            enterButton="Search"
+            size="large"
+            value={searchQuery}
+            onSearch={(value) =>
+              (window.location.href = `/search?q=${encodeURIComponent(value)}`)
+            }
+          />
+          <Navbar.Toggle />
+        </Navbar.Container>
+      </Navbar.Container>
+    </Navbar>
   );
 }
