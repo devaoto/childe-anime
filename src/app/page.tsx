2feb590712d676a77665b015ec5354b1a89a37a7
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-02-06 21:00:22 +0600

    Sort all components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import Image from 'next/image';
 import Trending from '../components/trending';
 import NavBar from '@/components/navbar';
+import Recent from '@/components/recent';
+import Popular from '@/components/popular';
+import Random from '@/components/random';
 
 export default function Home() {
   return (
     <div className="overflow-x-hidden">
       <NavBar />
+      <Popular />
       <Trending />
+      <Random />
+      <Recent />
     </div>
   );
 }
