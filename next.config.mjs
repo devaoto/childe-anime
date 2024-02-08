a495f0e515f19c8f0b1cf920fa1746655e779ddd
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-02-08 17:13:39 +0600

    Add popover

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'media.kitsu.io',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { motion } from 'framer-motion';
-import { Tooltip } from 'antd';
+import { ConfigProvider, Popover } from 'antd';
 import Image from 'next/image';
 import { Card } from 'keep-react';
 import { useRouter } from 'next/navigation';
@@ -78,70 +78,119 @@ const SwiperComponent: React.FC<SwiperComponentProps> = ({
       style={{ width: 'auto' }}
       className="flex flex-wrap w-full gap-3"
     >
-      {animeList.map((anime, index) => (
-        <SwiperSlide style={{ width: 'auto' }} key={index}>
-          <motion.div
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-            onClick={() => handleCardClick(anime.id as string, index)}
-          >
-            {isMobile ? (
-              <div>
-                <Tooltip
-                  title={
-                    <h1 className="text-xs">
-                      {anime.title?.english
-                        ? anime.title.english
-                        : anime.title?.romaji}
-                    </h1>
-                  }
-                  placement="top"
-                  visible={tooltipVisible[index]}
+      {animeList.map((anime, index) => {
+        const content = (
+          <div>
+            <p className="text-white">Total Episodes: {anime.totalEpisodes}</p>
+            <p className="text-white">Rating: {anime.rating}%</p>
+          </div>
+        );
+
+        return (
+          <SwiperSlide style={{ width: 'auto' }} key={index}>
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              onClick={() => handleCardClick(anime.id as string, index)}
+            >
+              {isMobile ? (
+                <div>
+                  <ConfigProvider
+                    theme={{
+                      token: {
+                        colorBgElevated: '#222222',
+                        colorText: '#ffffff',
+                        colorTextHeading: '#ffffff',
+                      },
+                    }}
+                  >
+                    <Popover
+                      title={
+                        <h1 className="text-xs">
+                          {anime.title?.english
+                            ? anime.title.english
+                            : anime.title?.romaji}
+                        </h1>
+                      }
+                      content={content}
+                      placement="top"
+                      visible={tooltipVisible[index]}
+                    >
+                      <Image
+                        src={anime.image as string}
+                        alt={`${anime.title?.native}`}
+                        width={500}
+                        height={500}
+                        objectFit="cover"
+                        className="h-auto w-full object-cover select-none"
+                        style={{ height: 200, width: 600 }}
+                      />
+                    </Popover>
+                  </ConfigProvider>
+                </div>
+              ) : (
+                <ConfigProvider
+                  theme={{
+                    token: {
+                      colorBgElevated: '#222222',
+                      colorText: '#ffffff',
+                      colorTextHeading: '#ffffff',
+                    },
+                  }}
                 >
-                  <Image
-                    src={anime.image as string}
-                    alt={`${anime.title?.native}`}
-                    width={500}
-                    height={500}
-                    objectFit="cover"
-                    className="h-auto w-full object-cover select-none"
-                    style={{ height: 200, width: 600 }}
-                  />
-                </Tooltip>
-              </div>
-            ) : (
-              <Card
-                className="max-w-xs overflow-hidden rounded-md max-h-[80%] min-h-[300px] h-xs lg:w-4/5 lg:h-4/5 md:h-3/5 md:w-full"
-                imgSrc={`${anime.image}`}
-                imgSize={'md'}
-                imgAlt={`${anime.title?.native}`}
-              >
-                <Card.Container className="my-3">
-                  <Card.Title>
-                    <h1 className="text-[12px] lg:text-sm md:text-xs">
-                      {' '}
-                      {anime?.title?.english
-                        ? anime.title.english.length > 35
-                          ? `${anime.title.english.slice(0, 35)}...`
-                          : anime.title.english
-                        : (anime.title?.romaji?.length as number) > 35
-                        ? `${anime.title?.romaji?.slice(0, 35)}`
-                        : `${anime.title?.romaji}`}
-                    </h1>
-                  </Card.Title>
-                  {includeEpisodeList ? (
-                    <Card.Description>
-                      Total episodes: {anime?.totalEpisodes}
-                    </Card.Description>
-                  ) : (
-                    <Card.Description>Watch now</Card.Description>
-                  )}
-                </Card.Container>
-              </Card>
-            )}
-          </motion.div>
-        </SwiperSlide>
-      ))}
+                  <Popover
+                    title={
+                      <h1>
+                        Information related to{' '}
+                        {anime.title?.english
+                          ? anime.title.english
+                          : anime.title?.romaji}
+                      </h1>
+                    }
+                    content={
+                      <div>
+                        <p>Status {anime.status}</p>
+                        <p>Rating: {anime.rating}</p>
+                        <p>Released: {anime.releaseDate}</p>
+                      </div>
+                    }
+                    trigger={'click'}
+                  >
+                    <Card
+                      className="max-w-xs overflow-hidden rounded-md max-h-[80%] min-h-[300px] h-xs lg:w-4/5 lg:h-4/5 md:h-3/5 md:w-full"
+                      imgSrc={`${anime.image}`}
+                      imgSize={'md'}
+                      imgAlt={`${anime.title?.native}`}
+                    >
+                      <Card.Container className="my-3">
+                        <Card.Title>
+                          <h1 className="text-[12px] lg:text-sm md:text-xs">
+                            {' '}
+                            {anime?.title?.english
+                              ? anime.title.english.length > 35
+                                ? `${anime.title.english.slice(0, 35)}...`
+                                : anime.title.english
+                              : (anime.title?.romaji?.length as number) > 35
+                              ? `${anime.title?.romaji?.slice(0, 35)}`
+                              : `${anime.title?.romaji}`}
+                          </h1>
+                        </Card.Title>
+                        {includeEpisodeList ? (
+                          <Card.Description>
+                            Total episodes: {anime?.totalEpisodes}
+                          </Card.Description>
+                        ) : (
+                          <Card.Description>Watch now</Card.Description>
+                        )}
+                      </Card.Container>
+                    </Card>
+                  </Popover>
+                </ConfigProvider>
+              )}
+            </motion.div>
+          </SwiperSlide>
+        );
+      })}
     </Swiper>
   );
 };
