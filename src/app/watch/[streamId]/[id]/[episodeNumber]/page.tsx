08b981fa347992aa8582f043feca8070c7bbd157
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-02-21 18:12:17 +0600

    Use Anify

--- src/app/watch/[streamId]/[id]/[episodeNumber]/page.tsx
+++ src/app/watch/[streamId]/[id]/[episodeNumber]/page.tsx
@@ -2,180 +2,18 @@
 
 import React, { useEffect, useState } from 'react';
 import { Tooltip } from 'antd';
-import Image from 'next/image';
 import Footer from '@/components/footer';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Mousewheel } from 'swiper/modules';
+import 'swiper/css';
 
-interface Anime {
-  id: string;
-  title: {
-    romaji: string;
-    english: string | null;
-    native: string;
-  };
-  malId: number;
-  synonyms: string[];
-  isLicensed: boolean;
-  isAdult: boolean;
-  countryOfOrigin: string;
-  trailer: {
-    id: string;
-    site: string;
-    thumbnail: string;
-    thumbnailHash: string;
-  };
-  image: string;
-  imageHash: string;
-  popularity: number;
-  color: string;
-  cover: string;
-  coverHash: string;
-  description: string;
-  status: string;
-  releaseDate: number;
-  startDate: {
-    year: number;
-    month: number;
-    day: number;
-  };
-  endDate: {
-    year: number;
-    month: number;
-    day: number;
-  };
-  totalEpisodes: number;
-  currentEpisode: number;
-  rating: number;
-  duration: number;
-  genres: string[];
-  season: string;
-  studios: string[];
-  subOrDub: string;
-  type: string;
-  recommendations: Recommendation[];
-  characters: Character[];
-  relations: Relation[];
-  episodes: Episode[];
-}
-
-interface Recommendation {
-  id: number;
-  malId: number;
-  title: {
-    romaji: string;
-    english: string;
-    native: string;
-    userPreferred: string;
-  };
-  status: string;
-  episodes: number;
-  image: string;
-  imageHash: string;
-  cover: string;
-  coverHash: string;
-  rating: number;
-  type: string;
-}
-
-interface Character {
-  id: number;
-  role: string;
-  name: {
-    first: string;
-    last: string | null;
-    full: string;
-    native: string;
-    userPreferred: string;
-  };
-  image: string;
-  imageHash: string;
-  voiceActors: VoiceActor[];
-}
-
-interface VoiceActor {
-  id: number;
-  language: string;
-  name: {
-    first: string;
-    last: string;
-    full: string;
-    native: string;
-    userPreferred: string;
-  };
-  image: string;
-  imageHash: string;
-}
-
-interface Relation {
-  id: number;
-  relationType: string;
-  malId: number;
-  title: {
-    romaji: string;
-    english: string;
-    native: string;
-    userPreferred: string;
-  };
-  status: string;
-  episodes: number | null;
-  image: string;
-  imageHash: string;
-  color: string;
-  type: string;
-  cover: string;
-  coverHash: string;
-  rating: number;
-}
-
-interface Episode {
-  id: string;
-  title: string | null;
-  image: string;
-  imageHash: string;
-  number: number;
-  createdAt: string | null;
-  description: string | null;
-  url: string;
-}
-
-interface Source {
-  url: string;
-  quality: string;
-}
-
-interface Subtitle {
-  url: string;
-  lang: string;
-}
-
-interface IntroOutro {
-  start: number;
-  end: number;
-}
-
-interface VideoHeaders {
-  [key: string]: string;
-}
-
-interface VideoConfig {
-  sources: Source[];
-  subtitles: Subtitle[];
-  audio: any[];
-  intro: IntroOutro;
-  outro: IntroOutro;
-  headers: VideoHeaders;
-}
-
-interface AnifyEpisodeDetail {
-  id: string;
-  number: number;
-  title: string;
-  isFiller: boolean;
-  img: string | null;
-  hasDub: boolean;
-  description: string | null;
-  rating: number | null;
-  updatedAt: number;
-}
+import {
+  AnifyEpisodeDetail,
+  VideoConfig,
+  Episode,
+} from '@/lib/types/watch.types';
+import { sendRequest } from '@/lib/utils/request';
+import { AnimeAnilist, Anime } from '@/lib/types/details.types';
 
 type Props = {
   params: {
@@ -190,16 +28,79 @@ export default function Watch({ params }: Readonly<Props>) {
   const [buttonHoverStates, setButtonHoverStates] = useState<{
     [key: string]: boolean;
   }>({});
-  const [details, setDetails] = useState<Anime | null>(null);
+  const [details, setDetails] = useState<Anime | AnimeAnilist | null>(null);
   const [episodeLinks, setEpisodeLinks] = useState<VideoConfig | null>(null);
   const [encodedIframeData, setEncodedIframeData] = useState<any>(null);
-  const [episodes, setEpisodes] = useState<AnifyEpisodeDetail[] | null>(null);
-  const [universialProvider, setUniProvider] = useState<
+  const [episodes, setEpisodes] = useState<
+    AnifyEpisodeDetail[] | Episode[] | null
+  >(null);
+  const [universalProvider, setUniProvider] = useState<
     'gogoanime' | 'zoro' | null
   >(null);
-  const [unisersialServer, setUniServer] = useState<
+  const [universalServer, setUniServer] = useState<
     'vidcloud' | 'gogocdn' | null
   >(null);
+  const [uniqueCharacters, setUniqueCharacters] = useState<any>({});
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(`https://api.anify.tv/info/${params.id}`);
+        const data = await response.json();
+        setDetails(data);
+      } catch (error) {
+        const response = await fetch(
+          `https://consumet-api-h1ga.onrender.com/meta/anilist/info/${params.id}`
+        );
+        const data = await response.json();
+
+        setDetails(data);
+      }
+    })();
+  }, [params.id]);
+  useEffect(() => {
+    (async () => {
+      try {
+        const zoroProvider = (details as Anime).episodes.data.find(
+          (provider: { providerId: string }) => provider.providerId === 'zoro'
+        );
+        const gogoProvider = (details as Anime).episodes.data.find(
+          (provider: { providerId: string }) =>
+            provider.providerId === 'gogoanime'
+        );
+
+        if (zoroProvider) {
+          setEpisodes(zoroProvider?.episodes);
+        } else if (gogoProvider) {
+          setEpisodes(gogoProvider?.episodes);
+        } else {
+          return <div>No provider found.</div>;
+        }
+      } catch (error) {
+        const response = await fetch(
+          `https://api.anify.tv/episodes/${params.id}`
+        );
+
+        const results = await response.json();
+
+        const zoroProvider = results.find(
+          (provider: { providerId: string }) => provider.providerId === 'zoro'
+        );
+        const gogoProvider = results.find(
+          (provider: { providerId: string }) =>
+            provider.providerId === 'gogoanime'
+        );
+
+        if (zoroProvider) {
+          setEpisodes(zoroProvider?.episodes);
+        } else if (gogoProvider) {
+          setEpisodes(gogoProvider?.episodes);
+        } else {
+          return <div>No provider found.</div>;
+        }
+      }
+    })();
+  }, [params.id, details]);
 
   useEffect(() => {
     let provider;
@@ -211,70 +112,87 @@ export default function Watch({ params }: Readonly<Props>) {
       provider = 'gogoanime';
       server = 'gogocdn';
     }
-    console.log(
-      decodeURIComponent(params.streamId).split('/').includes('watch')
-    );
 
     (async () => {
-      setUniProvider(provider as 'gogoanime' | 'zoro');
-      setUniServer(server as 'gogocdn' | 'vidcloud');
-      const streamId = params.streamId;
-      const response = await fetch(
-        `https://api.anify.tv/sources?providerId=${provider}&watchId=${streamId}&episodeNumber=${params.episodeNumber}&id=${params.id}&subType=sub&server=${server}`
-      );
+      try {
+        setUniProvider(provider as 'gogoanime' | 'zoro');
+        setUniServer(server as 'gogocdn' | 'vidcloud');
+        const streamId = params.streamId;
+        const episodes = await sendRequest(
+          `https://api.anify.tv/sources?providerId=${provider}&watchId=${streamId}&episodeNumber=${params.episodeNumber}&id=${params.id}&subType=sub&server=${server}`
+        );
 
-      const episodes = await response.json();
+        setEpisodeLinks(episodes);
+      } catch (error) {
+        if (details && details?.episodes) {
+          try {
+            const episodesList = (details as Anime).episodes.data.find(
+              (provider) => provider.providerId === 'gogoanime'
+            )?.episodes;
 
-      setEpisodeLinks(episodes);
-    })();
-  }, [params.streamId, params.id, params.episodeNumber]);
+            console.log('params.episodeNumber', params.episodeNumber);
+            console.log('episodesList', episodesList);
 
-  useEffect(() => {
-    (async () => {
-      const response = await fetch(
-        `https://consumet-api-h1ga.onrender.com/meta/anilist/info/${params.id}`
-      );
-      const result = await response.json();
-      setDetails(result);
-    })();
-  }, [params.id]);
-  useEffect(() => {
-    (async () => {
-      const response = await fetch(
-        `https://api.anify.tv/episodes/${params.id}`
-      );
+            const episodeSelected = episodesList?.find((episode) => {
+              console.log('episode.number:', episode);
+              console.log('params.episodeNumber:', params.episodeNumber);
+              console.log('typeof episode.number', typeof episode.number);
+              console.log(
+                'typeof params.episodeNumber',
+                typeof params.episodeNumber
+              );
+              return episode.number === Number(params.episodeNumber);
+            });
 
-      const results = await response.json();
+            if (episodeSelected) {
+              const result = await sendRequest(
+                `https://api.amvstr.me/api/v2/stream/${decodeURIComponent(
+                  episodeSelected?.id as string
+                ).replace(/\//g, '')}`
+              );
 
-      const zoroProvider = results.find(
-        (provider: { providerId: string }) => provider.providerId === 'zoro'
-      );
+              setEpisodeLinks(null);
+              setUniProvider('gogoanime');
+              setUniServer('gogocdn');
+              setData(result);
+            }
+          } catch (error) {
+            const episodesList = (details as AnimeAnilist)?.episodes;
 
-      const gogoProvider = results.find(
-        (provider: { providerId: string }) =>
-          provider.providerId === 'gogoanime'
-      );
+            console.log('params.episodeNumber', params.episodeNumber);
+            console.log('episodesList', episodesList);
 
-      if (zoroProvider) {
-        setEpisodes(zoroProvider?.episodes);
-      } else if (gogoProvider) {
-        setEpisodes(gogoProvider?.episodes);
-      } else {
-        return <div>No provider found.</div>;
+            const episodeSelected = episodesList?.find(
+              (episode) => episode.number === params.episodeNumber
+            );
+            if (episodeSelected) {
+              const result = await sendRequest(
+                `https://api.amvstr.me/api/v2/stream/${decodeURIComponent(
+                  episodeSelected?.id as string
+                ).replace(/\//g, '')}`
+              );
+
+              setEpisodeLinks(null);
+              setUniProvider('gogoanime');
+              setUniServer('gogocdn');
+              setData(result);
+            }
+          }
+        }
       }
     })();
-  }, [params.id]);
+  }, [params.streamId, params.id, params.episodeNumber, details]);
 
   useEffect(() => {
     (async () => {
       if (episodeLinks && episodeLinks?.sources && episodeLinks?.subtitles) {
-        if (universialProvider === 'zoro') {
+        if (universalProvider === 'zoro') {
           const autoQualitySource = episodeLinks?.sources.find(
             (source) => source.quality === 'auto'
           );
 
           if (autoQualitySource) {
-            const response = await fetch(
+            const result = await sendRequest(
               `https://childe-anime-player.vercel.app/plyr/iframe`,
               {
                 method: 'POST',
@@ -304,24 +222,35 @@ export default function Watch({ params }: Readonly<Props>) {
                 }),
               }
             );
-            const result = await response.text();
             setEncodedIframeData(`${result}`);
           }
         } else {
           (async () => {
-            const response = await fetch(
+            const result = await sendRequest(
               `https://api.amvstr.me/api/v2/stream/${decodeURIComponent(
                 params.streamId
               ).replace(/\//g, '')}`
             );
-            const result = await response.json();
-
             setData(result);
           })();
         }
       }
     })();
-  }, [details, episodeLinks, params.streamId, universialProvider]);
+  }, [details, episodeLinks, params.streamId, universalProvider]);
+
+  useEffect(() => {
+    if (details && (details as Anime).characters) {
+      const uniqueChars: any = {};
+
+      (details as Anime).characters.forEach((char) => {
+        if (!uniqueChars[char.name]) {
+          uniqueChars[char.name] = char;
+        }
+      });
+
+      setUniqueCharacters(uniqueChars);
+    }
+  }, [details]);
 
   function darkenHexColor(hex: string, percent: number) {
     percent = Math.min(100, Math.max(0, percent));
@@ -395,32 +324,39 @@ export default function Watch({ params }: Readonly<Props>) {
               </span>
             </h1>
             <div className="flex flex-col gap-20 lg:flex-row">
-              {unisersialServer === 'gogocdn' ? (
-                <iframe
-                  src={data?.plyr?.main || data?.plyr?.backup}
-                  scrolling="no"
-                  frameBorder="0"
-                  allowFullScreen={true}
-                  title={params.streamId}
-                  allow="picture-in-picture"
-                  className="lg:w-2/4 md:w-3/4 sm:w-full  rounded-lg h-[30vh] lg:h-[50vh] md:h-[30vh]"
-                ></iframe>
+              {universalServer === 'gogocdn' ? (
+                data.plyr.main ? (
+                  <iframe
+                    src={data?.plyr?.main || data?.plyr?.backup}
+                    scrolling="no"
+                    frameBorder="0"
+                    allowFullScreen={true}
+                    title={params.streamId}
+                    allow="picture-in-picture"
+                    className="lg:w-2/4 md:w-3/4 sm:w-full rounded-lg h-[30vh] lg:h-[50vh] md:h-[30vh]"
+                  ></iframe>
+                ) : (
+                  <div>Loading...</div>
+                )
               ) : (
-                <iframe
-                  ref={(iframe) => {
-                    if (iframe) {
-                      iframe.contentDocument?.open();
-                      iframe.contentDocument?.write(encodedIframeData);
-                      iframe.contentDocument?.close();
-                    }
-                  }}
-                  scrolling="no"
-                  frameBorder="0"
-                  allowFullScreen={true}
-                  title={params.streamId}
-                  allow="autoplay; fullscreen; picture-in-picture; controls"
-                  className="w-full rounded-lg h-[50vh] lg:h-[75vh] md:h-[55vh]"
-                />
+                <>
+                  <div>Loading...</div>
+                  <iframe
+                    ref={(iframe) => {
+                      if (iframe) {
+                        iframe.contentDocument?.open();
+                        iframe.contentDocument?.write(encodedIframeData);
+                        iframe.contentDocument?.close();
+                      }
+                    }}
+                    scrolling="no"
+                    frameBorder="0"
+                    allowFullScreen={true}
+                    title={params.streamId}
+                    allow="autoplay; fullscreen; picture-in-picture; controls"
+                    className="w-full rounded-lg h-[50vh] lg:h-[75vh] md:h-[55vh]"
+                  />
+                </>
               )}
 
               <div className="flex flex-wrap gap-2 mt-4 max-h-[360px] max-w-[500px] overflow-y-auto">
@@ -481,38 +417,40 @@ export default function Watch({ params }: Readonly<Props>) {
                 </div>
               </div>
             </div>
-            <h1 className="mt-6 text-xl lg:text-4xl md:text-2xl sm:text-xl ">
-              Relations
-            </h1>
-            <div className="flex gap-3 mt-4">
-              {details?.relations.map((relation, value) => {
-                return (
-                  <a
-                    key={relation.id}
-                    href={`/details/${relation.id}`}
-                    className="cursor-pointer hover:scale-105 duration-300"
-                  >
-                    <Tooltip
-                      key={relation.id}
-                      title={
-                        <h1 className="text-xs">
-                          Watch {relation.title?.english}{' '}
-                          {relation.relationType}
-                        </h1>
-                      }
-                      placement="top"
-                    >
-                      <Image
-                        src={relation.image as string}
-                        alt={relation.title?.native as string}
-                        height={500}
-                        width={500}
-                        className="h-[150px] w-[90px] lg:h-[300px] lg:w-[150px] md:h-[250px] md:w-[100px] object-cover rounded-md select-none"
-                      />
-                    </Tooltip>
-                  </a>
-                );
-              })}
+          </div>
+          <div className="mt-10">
+            <h1 className="text-2xl mb-5">Voice Actors</h1>
+            <div className="w-full flex flex-wrap flex-row gap-5">
+              <Swiper
+                slidesPerView={10}
+                spaceBetween={20}
+                modules={[Mousewheel]}
+                mousewheel={{
+                  sensitivity: 50,
+                }}
+                direction="horizontal"
+                className="w-full"
+              >
+                {uniqueCharacters &&
+                  Object.values(uniqueCharacters).map((char: any) => (
+                    <SwiperSlide key={char.name}>
+                      <div className="flex flex-col max-w-48 max-h-48 justify-center items-center select-none">
+                        <img
+                          src={char.image}
+                          alt={`${char.name} - ${char.voiceActor.name}`}
+                          width={512}
+                          height={512}
+                          loading="lazy"
+                          className="object-cover rounded-full max-h-32 max-w-32"
+                        />
+                        <p className="text-sm font-semibold">{char.name}</p>
+                        <p className="text-xs font-semibold">
+                          {char.voiceActor.name}
+                        </p>
+                      </div>
+                    </SwiperSlide>
+                  ))}
+              </Swiper>
             </div>
           </div>
           <Footer />
