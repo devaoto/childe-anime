59e2515d22acb32caf9b36e2a3846fe50b48c795
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-02-21 19:37:24 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div className="overflow-x-hidden">
       <div>
-        <div className="z-[9999]">
+        <div className="relative z-[9999]">
           <NavBar />
         </div>
         <Popular />
