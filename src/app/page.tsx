09c220ba7ffa9e024f9b9b79dae7af87abd0503d
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-02-21 19:25:15 +0600

    Change the bg with the particles and the meteors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,32 @@ import Recent from '@/components/recent';
 import Popular from '@/components/popular';
 import Random from '@/components/random';
 import Footer from '@/components/footer';
+import { Meteors } from '@/components/meteors';
+import { SparklesCore } from '@/components/sparkles';
 
 export default function Home() {
   return (
     <div className="overflow-x-hidden">
-      <NavBar />
-      <Popular />
-      <Trending />
-      <Random />
-      <Recent />
-      <Footer />
+      <div>
+        <NavBar />
+        <Popular />
+        <Trending />
+        <Random />
+        <Recent />
+        <Footer />
+      </div>
+      <Meteors number={3} />
+      <div className="w-full absolute inset-0 h-full">
+        <SparklesCore
+          id="tsparticlesfullpage"
+          background="transparent"
+          minSize={0.6}
+          maxSize={1.4}
+          particleDensity={100}
+          className="w-full h-full"
+          particleColor="#008080"
+        />
+      </div>
     </div>
   );
 }
